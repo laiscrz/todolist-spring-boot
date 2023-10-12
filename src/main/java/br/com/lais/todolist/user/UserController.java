880a4bb045deff7a6b627d65116b378f10addca6
laiscrz <lais.albino2003@gmail.com>
2023-10-12 15:00:53 -0300

    Add IUserRepository + aprimorado 'create'

--- src/main/java/br/com/lais/todolist/user/UserController.java
+++ src/main/java/br/com/lais/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.lais.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,24 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    /*
-     * TIPOS - Revisão
-     * 
-     * String(texto)
-     * Integer (int) numeros inteiros
-     * Double (double) numeros com casas decimais, + precisao.
-     * Float (float)
-     * Char(A C)
-     * Date (data)
-     * void 
-     */
+  
+  @Autowired
+  private IUserRepository userRepository;
 
-    /*
-      * Body - Corpo
-    */
   @PostMapping("/")
-  public void create(@RequestBody UserModel userModel){
-    System.out.println(userModel.getUsername());
+  public UserModel create(@RequestBody UserModel userModel){
+    var userCreated = this.userRepository.save(userModel);
+    return userCreated;
   }
 
     

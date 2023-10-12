9b025fb458e9d4fb2a6801ecb6c3a7bb2973c21b
laiscrz <lais.albino2003@gmail.com>
2023-10-12 13:30:39 -0300

    criação de dependencia lombok

--- src/main/java/br/com/lais/todolist/user/UserModel.java
+++ src/main/java/br/com/lais/todolist/user/UserModel.java
@@ -1,33 +1,12 @@
 package br.com.lais.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
     private String username;
     private String name;
     private String password;
 
 
-    //getters setters
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 }

ada735baa706ebb2d4d6e9095ca398fdba76c409
laiscrz <lais.albino2003@gmail.com>
2023-10-12 16:38:23 -0300

    Add criptografia de senha com BCrypt

--- src/main/java/br/com/lais/todolist/user/UserController.java
+++ src/main/java/br/com/lais/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 /*
  * MODIFICADOR
  * 
@@ -33,6 +35,10 @@ public class UserController {
       //Status Code
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario ja existe");
     }
+
+    var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+    userModel.setPassword(passwordHashred);
 
     var userCreated = this.userRepository.save(userModel);
     return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);

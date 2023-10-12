985bad67b2b0612e077a0cedaabf937bc065060c
laiscrz <lais.albino2003@gmail.com>
2023-10-11 21:27:19 -0300

    add files spring boot

---
+++ src/main/java/br/com/laiscruz/todolist/controller/MinhaPrimeiraController.java
@@ -0,0 +1,10 @@
+package br.com.laiscruz.todolist.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class MinhaPrimeiraController {
+    
+}

0fceb77004c429c5f8e963c6d3ceef7d534442ff
laiscrz <lais.albino2003@gmail.com>
2023-10-11 22:05:30 -0300

    criação userController + seu conteudo inicial

---
+++ src/main/java/br/com/lais/todolist/user/UserController.java
@@ -0,0 +1,33 @@
+package br.com.lais.todolist.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * MODIFICADOR
+ * 
+ * public
+ * private
+ * protected
+ * 
+ */
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    /*
+     * TIPOS - Revisão
+     * 
+     * String(texto)
+     * Integer (int) numeros inteiros
+     * Double (double) numeros com casas decimais, + precisao.
+     * Float (float)
+     * Char(A C)
+     * Date (data)
+     * void 
+     */
+    public void create(){
+
+    }
+
+}

ff47827f8b626c7c7ac8f7a03a7bac00285bc750
laiscrz <lais.albino2003@gmail.com>
2023-10-12 00:34:50 -0300

    Criação de UserModel + Metodos em UserModel

--- src/main/java/br/com/lais/todolist/user/UserController.java
+++ src/main/java/br/com/lais/todolist/user/UserController.java
@@ -1,5 +1,7 @@
 package br.com.lais.todolist.user;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,8 +28,13 @@ public class UserController {
      * Date (data)
      * void 
      */
-    public void create(){
 
+    /*
+      * Body - Corpo
+    */
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel){
+        System.out.println(userModel.name);
     }
 
 }
---
+++ src/main/java/br/com/lais/todolist/user/UserModel.java
@@ -0,0 +1,7 @@
+package br.com.lais.todolist.user;
+
+public class UserModel {
+    public String username;
+    public String name;
+    public String passaword;
+}

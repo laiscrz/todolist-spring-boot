25f2bb8cf2aee8ebfe347569b66ce6c65a3fe45d
laiscrz <lais.albino2003@gmail.com>
2023-10-14 17:50:27 -0300

    Add validação para usuario dono

--- src/main/java/br/com/lais/todolist/task/TaskController.java
+++ src/main/java/br/com/lais/todolist/task/TaskController.java
@@ -56,14 +56,24 @@ public class TaskController {
 
     // http://localhost:8080/tasks/id_da_taaks
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request){
-        
+    public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request){
         
         var task = this.taskRepository.findById(id).orElse(null);
 
+        if(task == null){
+             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa não encontrada");
+        }
+
+        var idUser = request.getAttribute("idUser");
+
+        if (!task.getIdUser().equals(idUser)){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário não tem permissão para alterar essa tarefa");
+        }
+
         Utils.copyNullProperties(taskModel, task);
         
+        var taskUpdated = this.taskRepository.save(task);
         
-        return this.taskRepository.save(task);
+        return ResponseEntity.ok().body(taskUpdated);
     }
 }
